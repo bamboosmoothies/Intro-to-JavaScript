2e226ce00d647e0a0c2eb77380cd22aaff9ba6bd
Brandon Hach <58790036+bamboosmoothies@users.noreply.github.com>
2022-05-27 13:34:12 -0400

    commit

---
+++ Lesson 01 Variables/index.js
@@ -0,0 +1,9 @@
+/* 
+A variable is a container for storing data.
+A variable behaves as if it was the value that it contains.
+
+Two Steps:
+1. Declaration (var, let, const)
+2. Assignment (= assignment operator)
+*/
+
