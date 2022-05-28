2a19a4a0387c3329350b05fea130bc429711643d
Brandon Hach <58790036+bamboosmoothies@users.noreply.github.com>
2022-05-27 21:56:15 -0400

    commit

--- Lesson 01 Variables/index.js
+++ Lesson 01 Variables/index.js
@@ -7,3 +7,17 @@ Two Steps:
 2. Assignment (= assignment operator)
 */
 
+let firstName = "Brandon"; //Strings
+let age; //numbers
+age = 21;
+let student = true; //booleans
+
+age = age + 1;
+
+console.log("Hello", firstName);
+console.log("You are", age, "years old");
+console.log("Enrolled:", student);
+
+document.getElementById("p1").innerHTML = "Hello " + firstName;
+document.getElementById("p2").innerHTML = "You are " + age + " years old";
+document.getElementById("p3").innerHTML = "Enrolled: " + student
