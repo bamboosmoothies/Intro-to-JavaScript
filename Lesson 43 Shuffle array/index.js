58f4c67cbf44f80447402064e6af8a3e9e1ccb8f
Brandon Hach <58790036+bamboosmoothies@users.noreply.github.com>
2022-06-02 12:16:14 -0400

    commit

--- Lesson 43 Shuffle array/index.js
+++ Lesson 43 Shuffle array/index.js
@@ -5,13 +5,20 @@ shuffle(cards);
 cards.forEach((card) => console.log(card));
 
 function shuffle(array) {
+	//began at the end bc need array length
 	let currentIndex = array.length;
+
 	while (currentIndex != 0) {
+		//Store random index
 		let randomIndex = Math.floor(Math.random() * array.length);
+		//decrement array
 		currentIndex -= 1;
 
+		//"temporary array" created to store elements from array
 		let temp = array[currentIndex];
+		//refresh new array length
 		array[currentIndex] = array[randomIndex];
+		//store element into temp (array)
 		array[randomIndex] = temp;
 	}
 
